8cf24f7d1ad2624dee714a36308ddd9d3f3a21b1
Luke Reed <lmr0125@gmail.com>
2016-02-19 20:35:54 -0500

    Commit chapter 1, project 2

---
+++ c1/1.2/README.md
@@ -0,0 +1,9 @@
+### Chapter 1, Project 2
+
+Determine an Employee's Wage assuming:
+`wage = (hours_worked * rate) + (overtime_hours * (rate * 1.5))`
+
+User inputs:
+* rate
+* hours_worked
+* overtime_hours
---
+++ c1/1.2/wage.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+'''Determine a weekly paycheck given rate,
+hours worked, and overtime hours worked.
+Assumes overtime rate = normal rate * 1.5
+'''
+
+
+class Wage:
+    '''Wage class to calculate paycheck.'''
+    def __init__(self, rate, hours, over_hours):
+        self.rate = rate
+        self.hours = hours
+        self.over_rate = (rate * 1.5)
+        self.over_hours = over_hours
+
+    def calc(self):
+        '''Method to calculate wage.'''
+        return (self.rate * self.hours) + (self.over_rate * self.over_hours)
+
+
+def main():
+    '''Main code.'''
+    try:
+        rate = float(input("Hourly Rate: "))
+        hours = float(input("Hours Worked: "))
+        over_hours = float(input("Overtime Hours Worked: "))
+        this_wage = Wage(rate, hours, over_hours)
+        print('%.2f' % this_wage.calc())
+    except ValueError:
+        print("That doesn't look like a number. Try again.")
+        main()
+
+
+if __name__ == '__main__':
+    main()
